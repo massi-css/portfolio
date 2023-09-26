2c136aefcd315c438613a6f902dd29d8a21a5078
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2023-09-26 03:48:13 +0100

    fixed bugs

---
+++ app.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById("btn");
+var navlinks = document.querySelectorAll(".navlink");
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+const works = document.querySelectorAll(".new");
+btn.addEventListener("click", function () {
+  if (btn.className == "btn close") {
+    btn.className = "btn open";
+    btn.innerHTML = "see less";
+    for (let work of works) {
+      work.className = "new work more";
+    }
+  } else {
+    if (btn.className == "btn open") {
+      btn.className = "btn close";
+      btn.innerHTML = "see more";
+      for (let work of works) {
+        work.className = "new work nomore";
+      }
+    }
+  }
+});
+
+navlinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navlinks.forEach((link) => link.classList.remove("active-a"));
+    link.classList.add("active-a");
+  });
+});
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
